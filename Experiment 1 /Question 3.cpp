9616159d725c4533f7d95815917825d292f68797
sahil-exe-17 <sahillale06@gmail.com>
2025-09-09 19:54:22 +0530

    Create Question 3.cpp

---
+++ Experiment 1 /Question 3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Time {
+public:
+    int hours;
+    int minutes;
+    int seconds;
+
+public:
+    void accept() {
+        cout << "Enter hours: ";
+        cin >> hours;
+
+        cout << "Enter minutes: ";
+        cin >> minutes;
+
+        cout << "Enter seconds: ";
+        cin >> seconds;
+    }
+
+    int toSeconds() {
+        return (hours * 3600) + (minutes * 60) + seconds;
+    }
+
+    void displaySeconds() {
+        cout << "Total seconds: " << toSeconds() << endl;
+    }
+};
+
+int main() {
+    Time t;
+    t.accept();
+    t.displaySeconds();
+    return 0;
+}
