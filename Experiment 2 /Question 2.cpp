166ebb99407b0a0534af19b3dbf7105900a35923
sahil-exe-17 <sahillale06@gmail.com>
2025-09-09 20:09:32 +0530

    Update Question 2.cpp

--- Experiment 2 /Question 2.cpp
+++ Experiment 2 /Question 2.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
 using namespace std;
 
-class City {
-    string name;
-    int population;
+class Account {
+    int acc_no;
+    float balance;
 
 public:
     void getData() {
-        cout << "Enter city name: ";
-        cin >> name;
-        cout << "Enter population: ";
-        cin >> population;
+        cout << "Enter Account Number: ";
+        cin >> acc_no;
+        cout << "Enter Balance: ";
+        cin >> balance;
+    }
+
+    void Interest() {
+        {
+            balance = balance + (balance * 0.10);  // adding 10% interest
+        }
+
+    void display() {
+        cout << "Account No: " << acc_no << ", Balance: " << balance << endl;
     }
 };
 
 int main() {
-    City c[5];
-    cout << "Enter details of 5 cities:\n";
-    for (int i = 0; i < 5; i++) {
-        cout << "\nCity " << i + 1 << ":\n";
-        c[i].getData();
+    Account a[10];
+
+    cout << "Enter details of 10 accounts:\n";
+    for (int i = 0; i < 10; i++) {
+        cout << "\nAccount " << i + 1 << ":\n";
+        a[i].getData();
     }
 
-    int max = 0;
-    for (int i = 1; i < 5; i++) {
-        if (c[i].getPopulation() > c[max].getPopulation()) {
-            
+    cout << "\nAccounts with Balance >= 5000 (after adding 10% interest):\n";
+    for (int i = 0; i < 10; i++) {
+        if (a[i].Balance() >= 5000) {
+            a[i].addInterest();
+            a[i].display();
         }
     }
 
-    cout << "\nCity with highest population:\n";
-    c[maxIndex].display();
-
     return 0;
 }
+
